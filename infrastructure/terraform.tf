805b51207640aac597c19feed32a1810931b484a
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-03-16 12:44:31 +0900

    adding configuration for Stream Analytics

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -27,6 +27,12 @@ variable "stream_analytics_name" {}
 
 variable "stream_analytics_sku" {}
 
+variable "stream_analytics_cosmosdb_name" {}
+
+variable "stream_analytics_cosmosdb_collection_name_pattern" {}
+
+variable "stream_analytics_cosmosdb_partition_key" {}
+
 # CosmosDB 
 
 variable "cosmosdb_account_name" {}
@@ -53,25 +59,30 @@ resource "azurerm_resource_group" "test" {
   }
 }
 
-# Stream Analytics
+# Event Hub
 
-resource "azurerm_template_deployment" "test" {
-  name                = "stream-deployment-01"
+resource "azurerm_eventhub_namespace" "test" {
+  name                = "${var.event_hub_namespace}"
+  location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  template_body       = "${file("${path.cwd}/template/template.json")}"
+  sku                 = "Standard"
+  capacity            = 1
 
-  parameters {
-    // Stream Analytics parameter
-    name           = "${var.stream_analytics_name}"
-    location       = "${azurerm_resource_group.test.location}"
-    apiVersion     = "2017-04-01-preview"
-    sku            = "${var.stream_analytics_sku}"
-    jobType        = "Cloud"
-    streamingUnits = 1
+  tags {
+    environment = "${var.environment}"
   }
 
-  deployment_mode = "Incremental"
-  depends_on      = ["azurerm_resource_group.test", "azurerm_cosmosdb_account.test"]
+  depends_on = ["azurerm_resource_group.test"]
+}
+
+resource "azurerm_eventhub" "test" {
+  name                = "${var.event_hub_name}"
+  namespace_name      = "${azurerm_eventhub_namespace.test.name}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  partition_count     = 2
+  message_retention   = 1
+
+  depends_on = ["azurerm_eventhub_namespace.test"]
 }
 
 # Cosmos DB
@@ -94,4 +105,37 @@ resource "azurerm_cosmosdb_account" "test" {
   tags {
     environment = "${var.environment}"
   }
+
+  depends_on = ["azurerm_resource_group.test"]
+}
+
+# Stream Analytics
+
+resource "azurerm_template_deployment" "test" {
+  name                = "stream-deployment-01"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  template_body       = "${file("${path.cwd}/template/template.json")}"
+
+  parameters {
+    // Stream Analytics parameter
+    name       = "${var.stream_analytics_name}"
+    location   = "${azurerm_resource_group.test.location}"
+    apiVersion = "2016-03-01"
+    sku        = "${var.stream_analytics_sku}"
+    jobType    = "Cloud"
+
+    // streamingUnits = "1"
+    eventHubNameSpace              = "${azurerm_eventhub_namespace.test.name}"
+    eventHubSharedAccessPolicyName = "RootManageSharedAccessKey"                                // need investigate
+    eventHubSharedAccessKey        = "${azurerm_eventhub_namespace.test.default_primary_key}"
+    eventHubName                   = "${azurerm_eventhub.test.name}"
+    cosmosDBAccountName            = "${azurerm_cosmosdb_account.test.name}"
+    cosmosDBAccountKey             = "${azurerm_cosmosdb_account.test.primary_master_key}"
+    cosmosDBDatabaseName           = "${var.stream_analytics_cosmosdb_name}"
+    cosmosDBCollectionNamePattern  = "${var.stream_analytics_cosmosdb_collection_name_pattern}"
+    cosmosDBPartitionKey           = "${var.stream_analytics_cosmosdb_partition_key}"
+  }
+
+  deployment_mode = "Incremental"
+  depends_on      = ["azurerm_resource_group.test", "azurerm_cosmosdb_account.test", "azurerm_eventhub.test"]
 }
